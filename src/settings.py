a5af6a0128f2b18dbdcb431acbfd19727a7fe52a
shihyuuuuuuu <www.elmo20816@gmail.com>
2021-06-14 01:29:40 +0800

    Fix

--- src/settings.py
+++ src/settings.py
@@ -164,6 +164,6 @@ AWS_QUERYSTRING_AUTH = False
 AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
 AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = os.environ['S3_BUCKET_NAME']
-MEDIA_URL = "https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
+MEDIA_URL = f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media/"
 MEDIA_ROOT = os.path.join(f"https://{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com", 'media')
 # DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
